a603544df74c483b2dda65d568f627af5863cae2
Newtonius <newtonius235@gmail.com>
2018-01-21 00:48:49 -0500

    test

--- akashichub.com/index.php
+++ akashichub.com/index.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 <title>Akashic Hub</title>
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-<style>
-  body {
-    background:#222;
-  }
-</style>
+
+<link rel="stylesheet" href="/css/core.css" type="text/css" />
+
+<script src="/js/icon_change.js" async></script>
+
 </head>
 <body>
-  <center>
-  <img style="padding: 50px;" src="GFX/Under_Construction.png" widh="356" height="468" alt="" />
-  <h1 style="font-family: 'Montserrat', sans-serif; color: #fff;">Under construction, please come back soon for something really cool!</h1>
-  </center>
+  <div class="wrapper">
+    <?php include ($_SERVER['DOCUMENT_ROOT'].'/adds/nav.html') ?>
+
+    <span id="anchor"></span>
+    <div class="content-wrapper">
+      <div class="cell">
+        <p class="quote">
+          "I went to the woods because I wished to live deliberately, to front only the essential facts of life, and see if I could not learn what it had to teach, and not, when I came to die, discover that I had not lived." <br/> - Henry David Thoreau
+        </p>
+      </div>
+    </div>
+    <div class="site-footer">
+    </div>
+  </div>
 </body>
 </html>
---
+++ akashichub.com/repair.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Akashic Hub</title>
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<style>
+  body {
+    background:#222;
+  }
+</style>
+</head>
+<body>
+  <center>
+  <img style="padding: 50px;" src="GFX/Under_Construction.png" widh="356" height="468" alt="" />
+  <h1 style="font-family: 'Montserrat', sans-serif; color: #fff;">Under construction, please come back soon for something really cool!</h1>
+  </center>
+</body>
+</html>
