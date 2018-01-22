5865f292a1b2da797404cb64325d6b10df9fec4b
Newtonius <newtonius235@gmail.com>
2018-01-23 03:43:48 +1100

    updates

--- akashichub.com/index.php
+++ akashichub.com/index.php
@@ -23,6 +23,19 @@
           "I went to the woods because I wished to live deliberately, to front only the essential facts of life, and see if I could not learn what it had to teach, and not, when I came to die, discover that I had not lived." <br/> - Henry David Thoreau
         </p>
       </div>
+
+      <div class="cell">
+        <p class="welcome">Welcome</p>
+        <div>
+
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+      </div>
     </div>
     <div class="site-footer">
     </div>
