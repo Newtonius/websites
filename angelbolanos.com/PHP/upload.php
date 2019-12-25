1eb47ea41d1fa2fd8627a9b3b5d7673807eb8fee
Newtonius <newtonius235@gmail.com>
2019-12-25 18:06:04 -0500

    upload code test

---
+++ angelbolanos.com/PHP/upload.php
@@ -0,0 +1,52 @@
+<?php
+// $target_dir = "uploads/" - specifies the directory where the file is going to be placed
+$target_dir = "uploads/";
+// $target_file specifies the path of the file to be uploaded
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $uploadOk=1 is not used yet (will be used later)
+$uploadOk = 1;
+// $imageFileType holds the file extension of the file (in lower case)
+$imageFileType = strtolower(pathinfo($target_file),PATHINFO_EXTENSION);
+
+// Check image file to determine if fake or real
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["name"]);
+  if($check == false) {
+    echo "tmp File Uploaded" . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "tmp File not uploaded";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "File already exists";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "File Exceeds limit";
+  $uploadOk = 0;
+}
+
+// Allow specific file formats
+if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png" && $imageFileType != "mp4") {
+  echo "File is wrong format: jpg, jpeg, png, and mp4 file types allowed only.";
+}
+
+// Final upload check
+if ($uploadOk == 0) {
+  echo "File was not uploaded";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "the file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+  } else {
+    echo "Error uploading cleared tmp file"
+  }
+}
+
+
+?>
--- angelbolanos.com/index.php
+++ angelbolanos.com/index.php
@@ -12,9 +12,11 @@
   </head>
   <body>
 
-    <?php
-      phpinfo();
-    ?>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select images to upload:
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
 
   </body>
 </html>
